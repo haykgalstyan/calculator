ca42fcb14323fdf10d1d4bf8c2f614964403ec94
Hayk Galstyan <hgalstyan@universe.dart.spb@hgalstyan.nb.ipa.dataart.net>
2021-06-29 15:09:58 +0400

    refactor

--- app/src/test/java/galstyan/hayk/calculator/SimpleCalculatorUnitTest.kt
+++ app/src/test/java/galstyan/hayk/calculator/SimpleCalculatorUnitTest.kt
@@ -11,57 +11,93 @@ import java.math.BigDecimal
 
 class SimpleCalculatorUnitTest {
 
-    private val calculator = CalculatorSimple()
 
     @Test
-    fun addition_isCorrect() = runBlocking {
-        calculator.add(Add(BigDecimal.valueOf(10)))
-        calculator.add(Add(BigDecimal.valueOf(5)))
-        val result = calculator.executeWith(BigDecimal.valueOf(5))
+    fun addition_isCorrect() {
+        CalculatorSimple().apply {
 
-        // 10 + 5 + 5 = 20
-        assertEquals(result, BigDecimal.valueOf(20))
+            add(Add(BigDecimal.valueOf(10)))
+            add(Add(BigDecimal.valueOf(5)))
+            val result = executeWith(BigDecimal.valueOf(5))
+
+            // 10 + 5 + 5 = 20
+            assertEquals(result, BigDecimal.valueOf(20))
+        }
+    }
+
+
+    @Test
+    fun subtraction_isCorrect() {
+        CalculatorSimple().apply {
+
+            add(Subtract(BigDecimal.valueOf(20)))
+            add(Subtract(BigDecimal.valueOf(5)))
+            val result = executeWith(BigDecimal.valueOf(5))
+
+            // 20 - 5 - 5 = 10
+            assertEquals(result, BigDecimal.valueOf(10))
+        }
+    }
+
+
+    @Test
+    fun multiplication_isCorrect() {
+        CalculatorSimple().apply {
+
+            add(Multiply(BigDecimal.valueOf(10)))
+            add(Multiply(BigDecimal.valueOf(10)))
+            val result = executeWith(BigDecimal.valueOf(10))
+
+            // 10 * 10 * 10 = 1000
+            assertEquals(result, BigDecimal.valueOf(1000))
+        }
     }
 
     @Test
-    fun subtraction_isCorrect() = runBlocking {
-        calculator.add(Subtract(BigDecimal.valueOf(20)))
-        calculator.add(Subtract(BigDecimal.valueOf(5)))
-        val result = calculator.executeWith(BigDecimal.valueOf(5))
+    fun division_isCorrect() {
+        CalculatorSimple().apply {
 
-        // 20 - 5 - 5 = 10
-        assertEquals(result, BigDecimal.valueOf(10))
+            add(Divide(BigDecimal.valueOf(1000)))
+            add(Divide(BigDecimal.valueOf(10)))
+            val result = executeWith(BigDecimal.valueOf(10))
+
+            // 1000 / 10 / 10 = 10
+            assertEquals(result, BigDecimal.valueOf(10))
+        }
     }
+
 
     @Test
-    fun multiplication_isCorrect() = runBlocking {
-        calculator.add(Multiply(BigDecimal.valueOf(10)))
-        calculator.add(Multiply(BigDecimal.valueOf(10)))
-        val result = calculator.executeWith(BigDecimal.valueOf(10))
+    fun allOperations_isCorrect() {
+        CalculatorSimple().apply {
 
-        // 10 * 10 * 10 = 1000
-        assertEquals(result, BigDecimal.valueOf(1000))
+            add(Add(BigDecimal.valueOf(10)))
+            add(Subtract(BigDecimal.valueOf(10)))
+            add(Multiply(BigDecimal.valueOf(10)))
+            add(Divide(BigDecimal.valueOf(10)))
+            val result = executeWith(BigDecimal.valueOf(10))
+
+            // 10 + 10 - 10 * 10 / 10 = 10
+            assertEquals(result, BigDecimal.valueOf(10))
+        }
     }
+
 
     @Test
-    fun division_isCorrect() = runBlocking {
-        calculator.add(Divide(BigDecimal.valueOf(1000)))
-        calculator.add(Divide(BigDecimal.valueOf(10)))
-        val result = calculator.executeWith(BigDecimal.valueOf(10))
+    fun clear_isCorrect() {
+        CalculatorSimple().apply {
 
-        // 1000 / 10 / 10 = 10
-        assertEquals(result, BigDecimal.valueOf(10))
+            add(Add(BigDecimal.valueOf(10)))
+            add(Add(BigDecimal.valueOf(10)))
+            add(Add(BigDecimal.valueOf(10)))
+            add(Add(BigDecimal.valueOf(10)))
+            clear()
+            add(Add(BigDecimal.valueOf(10)))
+            val result = executeWith(BigDecimal.valueOf(10))
+
+            // 10 + 10 = 20
+            assertEquals(result, BigDecimal.valueOf(20))
+        }
     }
 
-    @Test
-    fun division_allOperations() = runBlocking {
-        calculator.add(Add(BigDecimal.valueOf(10)))
-        calculator.add(Subtract(BigDecimal.valueOf(10)))
-        calculator.add(Multiply(BigDecimal.valueOf(10)))
-        calculator.add(Divide(BigDecimal.valueOf(10)))
-        val result = calculator.executeWith(BigDecimal.valueOf(10))
-
-        // 10 + 10 - 10 * 10 / 10 = 10
-        assertEquals(result, BigDecimal.valueOf(10))
-    }
 }
